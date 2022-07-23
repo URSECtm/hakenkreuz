a927db9f779bec5649148caa11a1cd36ed1f1cdd
prometheusZIP <91734001+prometheusZIP@users.noreply.github.com>
2022-07-24 03:02:06 +0500

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,22 @@
+#Point(x=621, y=843)
+#aray
+
+
+
+
+from pyautogui import * 
+currentMouseX, currentMouseY = position()
+browuserPosit = [547, 850]
+moveTo(browuserPosit)
+click(browuserPosit)
+dot1 = [ 850, 440]
+moveTo(dot1)
+drag(-150, 0, 0.5, button = "left")
+drag(0, 300, 0.5, button = "left")
+drag(-150, 0, 0.5, button = "left")
+move(0, -300)
+drag(0, 150, 0.5, button = "left")
+drag(300, 0, 0.5, button = "left")
+drag(0, 150, 0.5, button = "left")
+print(position())
+print(currentMouseX)
